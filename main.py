819a93e85068ff88549265965eb45f884713641b
rah-1 <65043687+rah-1@users.noreply.github.com>
2022-06-30 22:20:42 +0900

    add input validation and more informative comments

--- main.py
+++ main.py
@@ -15,15 +15,18 @@ class MyApp(App):
         self._curr_name = ""
         self._curr_cost = ""
 
-        # here: reads in old data upon build start
+        # reads in old data from csv upon build start
+        # stores Food objects in food_list
         self._food_list = []
         self.read_old_data()
 
+        # boolean variable to switch between food and cost entry
         self._first_click = False
 
         self._today = date.today()
-        print("Today's date:", self._today.strftime("%B %d, %Y"))
+        # print("Today's date:", self._today.strftime("%B %d, %Y"))
 
+        # layout convention: grid
         self.window = GridLayout()
         self.window.cols = 1
         self.window.size_hint = (0.6, 0.7)
@@ -32,51 +35,48 @@ class MyApp(App):
         # todo: logo widget
         # self.window.add_widget(Image(source="filename.png"))
 
-        # label widget
-        self.greeting = Label(
+        # label widget for the header
+        # also, color scheme can be changed... preferences?
+        self.header = Label(
             text="EAT MONEY\n" + self._today.strftime("%B %d, %Y"),
             font_size=36,
             color='#00FFCE',
             halign='center'
         )
-        self.window.add_widget(self.greeting)
+        self.window.add_widget(self.header)
 
-        # text input widget
+        # text input widget for user input
         self.input_field = TextInput(
             multiline=False,
             padding_y=(20, 20),
             size_hint=(1, 0.5)
         )
-
         self.window.add_widget(self.input_field)
 
-        # button widget
+        # button widget to submit text entry
         self.submit_button = Button(
             text="SUBMIT FOOD",
             size_hint=(1, 0.5),
             bold=True,
             background_color='#00FFCE',
-            # remove darker overlay of background colour
-            # background_normal = ""
         )
         self.submit_button.bind(on_press=self.big_button_press)
         self.window.add_widget(self.submit_button)
 
-        # button widget
+        # button widget to access stats (implementation currently tentative)
         self.stats_button = Button(
             text="VIEW STATS",
             size_hint=(1, 0.5),
             bold=True,
             background_color='#00FFCE',
-            # remove darker overlay of background colour
-            # background_normal = ""
         )
         self.stats_button.bind(on_press=self.view_stats_button)
         self.window.add_widget(self.stats_button)
 
-        # label widget
+        # label widget to display important info
+        # ex. if user input is invalid/successful
         self.infobox = Label(
-            text="some info here!",
+            text="Welcome to Eat Money!",
             font_size=14,
             color='#00FFCE',
             halign='center'
@@ -85,7 +85,8 @@ class MyApp(App):
 
         return self.window
 
-    # todo: this is a function to read in the csv file to load old data
+    # this is a function to read in the csv file to load old data
+    # it turns the csv rows into Food objects and stores them in foot_list
     def read_old_data(self):
         with open("data.csv") as csv_file:
             csv_reader = csv.reader(csv_file, delimiter=',')
@@ -102,31 +103,54 @@ class MyApp(App):
 
                 line_count += 1
 
+    # this function is for when we want to write new data
+    # it is invoked when we submit food/cost info
     def add_new_data(self, data):
         with open('data.csv', 'a', newline='') as f:
             writer = csv.writer(f)
             writer.writerow(data)
 
+    # input validation for cost field entry
+    # checks whether a valid float was entered
+    def check_valid_cost(self, entry):
+        try:
+            cost = float(entry)
+            return True
+        except ValueError:
+            self.infobox.text = "Please enter a valid cost!"
+            return False
 
+    # this function corresponds to the behavior when we click
+    # the topmost button (its name will change upon selection,
+    # so I've decided to call it "big button")
     def big_button_press(self, instance):
+        # the if-else corresponds to which button is currently "up"
+        # this first (if) block runs when we are at the submit cost menu
+        # the second (else) block runs when we are at the submit food menu
         if self._first_click:
-            self.submit_button.text = "SUBMIT FOOD"
-            self._curr_cost = self.input_field.text
-            self._first_click = False
+            if self.check_valid_cost(self.input_field.text):
+                self.submit_button.text = "SUBMIT FOOD"
+                self._curr_cost = self.input_field.text
+                self._first_click = False
 
-            # add food
-            curr_food = Food(self._today.strftime("%d/%m/%Y"), self._curr_name, self._curr_cost, 100)
-            data_entry = [self._today.strftime("%d/%m/%Y"), self._curr_name, self._curr_cost, 100]
-            self._food_list.append(curr_food)
-            self.add_new_data(data_entry)
+                # add Food object
+                curr_food = Food(self._today.strftime("%d/%m/%Y"), self._curr_name, self._curr_cost, 100)
+                data_entry = [self._today.strftime("%d/%m/%Y"), self._curr_name, self._curr_cost, 100]
+                self.infobox.text = self._curr_name + " ($"  + self._curr_cost + ") added successfully!"
+                self._food_list.append(curr_food)
+                self.add_new_data(data_entry)
 
         else:
-            self.submit_button.text = "SUBMIT COST"
-            self._curr_name = self.input_field.text
-            self._first_click = True
+            if self.input_field.text == "":
+                self.infobox.text = "Please enter a valid name!"
+            else:
+                self.submit_button.text = "SUBMIT COST"
+                self._curr_name = self.input_field.text
+                self.infobox.text = "Please enter the cost of " + self._curr_name
+                self._first_click = True
         self.input_field.text = ""
 
-    # todo: when stats button is preessed (popup???)
+    # todo: when stats button is pressed (popup???)
     def view_stats_button(self, instance):
         pass
 
